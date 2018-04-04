5fcc41b485ae7bbc8fbb8857e9fa11e67424ec22
hoger Hachi <nidhogg399@gmail.com>
2018-04-04 14:09:06 +0800

    修改shell脚本

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,10 @@ git reset --hard origin/master
 git clean -f
 git pull
 git checkout master
-yarn
-npm run start
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "install npm packages..."
+yarn
+npm run build
+npm run start
 echo "Finished."
